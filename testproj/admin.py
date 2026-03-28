5cadeeb0b015428c1b78f745e85153cf49a2b970
x-Drian <axiang1822@gmail.com>
2026-03-28 14:17:47 +0800

    Update admin.py

--- testproj/admin.py
+++ testproj/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import DateEntry, BookingLog, NotesLog
+from .models import DateEntry, BookingInquiry, BookingLog, NotesLog
 
 admin.site.register(DateEntry)
+admin.site.register(BookingInquiry)
 admin.site.register(BookingLog)
 admin.site.register(NotesLog)
