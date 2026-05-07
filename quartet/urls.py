ec3a85dd357d102cb73afabf8a08c0b4775d5bb8
x-Drian <axiang1822@gmail.com>
2026-05-08 00:51:36 +0800

    Fix: correctly linked testproj urls

--- quartet/urls.py
+++ quartet/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Change 'testproj' to 'testweb'
-    path('', include('testweb.urls')), 
+    path('', include('testproj.urls')), 
 ]
