6f670f1d94c080527d2fcf548c32b7b404ceac29
x-Drian <axiang1822@gmail.com>
2026-05-07 23:39:31 +0800

    Fix: updated main urls.py to include testweb urls and corrected project naming

--- quartet/urls.py
+++ quartet/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('testproj.urls')),
+    # Change 'testproj' to 'testweb'
+    path('', include('testweb.urls')), 
 ]
